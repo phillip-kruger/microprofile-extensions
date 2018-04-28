69c54c6c89cb99ea36da84393a4d3fceccfe42cd
Phillip Kruger <phillip.kruger@gmail.com>
2018-04-28 17:22:46 +0200

    Fixed system load. take number of processors into account

--- health-ext/src/main/java/com/github/phillipkruger/microprofileextentions/health/SystemLoadHealthCheck.java
+++ health-ext/src/main/java/com/github/phillipkruger/microprofileextentions/health/SystemLoadHealthCheck.java
@@ -12,6 +12,7 @@ import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 
 /**
  * Checking average load usage against configured max load
+ * @see http://blog.scoutapp.com/articles/2009/07/31/understanding-load-averages
  * @author Phillip Kruger (phillip.kruger@phillip-kruger.com)
  */
 @Health
@@ -31,17 +32,19 @@ public class SystemLoadHealthCheck implements HealthCheck {
         int availableProcessors = operatingSystemMXBean.getAvailableProcessors();
 
         double systemLoadAverage = operatingSystemMXBean.getSystemLoadAverage();
-
+        double systemLoadAveragePerProcessors = systemLoadAverage / availableProcessors;
+        
         HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("system-load")
                 .withData("name", name)
                 .withData("arch", arch)
                 .withData("version", version)
                 .withData("processors", availableProcessors)
                 .withData("loadAverage", String.valueOf(systemLoadAverage))
+                .withData("loadAverage per processor", String.valueOf(systemLoadAveragePerProcessors))
                 .withData("loadAverage %", String.valueOf(maxPercentage));
 
         if(systemLoadAverage>0){
-            boolean status = systemLoadAverage < maxPercentage;
+            boolean status = systemLoadAveragePerProcessors < maxPercentage;
             return responseBuilder.state(status).build();
         }else{
             // Load average not available
